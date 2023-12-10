21b3e7f4c22755ba4efda180165f287b63426d08
MbPro13_chudikO <oleg.chudnovskiy.gmail.com>
2023-12-10 20:47:04 +0200

    less17 add class CollectionAddressBook

---
+++ src/main/java/com/chudik0/addressbook/interfaces/impls/CollectionAddressBook.java
@@ -0,0 +1,45 @@
+package com.chudik0.addressbook.interfaces.impls;
+
+import com.chudik0.addressbook.interfaces.AddressBook;
+import com.chudik0.addressbook.objects.Person;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class CollectionAddressBook implements AddressBook {
+    private ArrayList<Person> personArrayList = new ArrayList<>();
+
+    @Override
+    public void addPerson(Person person) {
+        personArrayList.add(person);
+    }
+
+    @Override
+    public void editPerson(Person person) {
+
+    }
+
+    @Override
+    public void deletePerson(Person person) {
+        personArrayList.remove(person);
+    }
+
+    public ArrayList<Person> getPersonArrayList() {
+        return personArrayList;
+    }
+
+    public void fillTestData() {
+        Random random = new Random();
+        String[] names = {"John", "Alice", "Bob", "Emma", "Michael", "Olivia", "David", "Sophia", "James", "Emily"};
+        String[] lastNames = {"Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor"};
+
+        for (int i = 0; i < 20; i++) {
+            String firstName = names[random.nextInt(names.length)];
+            String lastName = lastNames[random.nextInt(lastNames.length)];
+            String phone = String.valueOf(random.nextInt(900000000) + 100000000);
+
+            Person person = new Person(firstName + " " + lastName, phone);
+            personArrayList.add(person);
+        }
+    }
+}
