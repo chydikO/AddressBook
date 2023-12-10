d56801ef24067cbb0204fb96c200ea127ebac3a0
Chudnovskiy.O.W520 <oleg.chudnovskiy@gmail.com>
2023-12-10 15:05:31 +0200

    less12, add main.fxml design

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.chudik0.addressbook {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.chudik0.addressbook to javafx.fxml;
+    exports com.chudik0.addressbook;
+}
