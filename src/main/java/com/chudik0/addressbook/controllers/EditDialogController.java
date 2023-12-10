4a586db485167a040489ea0902402b7b760ab093
MbPro13_chudikO <oleg.chudnovskiy.gmail.com>
2023-12-10 20:09:49 +0200

    less15 add FXML annotations in controllers

--- src/main/java/com/chudik0/addressbook/controllers/EditDialogController.java
+++ src/main/java/com/chudik0/addressbook/controllers/EditDialogController.java
@@ -1,6 +1,17 @@
 package com.chudik0.addressbook.controllers;
 
-public class EditDialogController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
+public class EditDialogController {
+    @FXML
+    public TextField textFieldName;
+    @FXML
+    public TextField textFieldPhone;
+    @FXML
+    public Button okButton;
+    @FXML
+    public Button cancelButton;
 
 }
--- src/main/java/com/chudik0/addressbook/controllers/MainController.java
+++ src/main/java/com/chudik0/addressbook/controllers/MainController.java
@@ -2,10 +2,15 @@ package com.chudik0.addressbook.controllers;
 
 import com.chudik0.addressbook.Main;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -13,6 +18,22 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class MainController {
+    @FXML
+    public Button addButton;
+    @FXML
+    public Button editButton;
+    @FXML
+    public Button deleteButton;
+    @FXML
+    public TextField textField;
+    @FXML
+    public Button searchButton;
+    @FXML
+    public TableView tableViewAddressBook;
+    @FXML
+    public Label labelCount;
+
+
     public void showDialog(ActionEvent actionEvent) throws IOException {
         Stage stage = new Stage();
         Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("edit-view.fxml")));
